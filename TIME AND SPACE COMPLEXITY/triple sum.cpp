79bb3e8b9882f5f0f1eed5b43909d460d4884bf9
Ravi Kumar <ravi14577@gmail.com>
2020-12-03 12:09:51 +0530

    arrar

--- TIME AND SPACE COMPLEXITY/triple sum.cpp
+++ TIME AND SPACE COMPLEXITY/triple sum.cpp
@@ -1,24 +1,50 @@
-/* 
-time complexity O(N^2)
-SPACE complexity O(N)
-WHERE N  IS SIZE OF ARRAY
-*/
-
-
-int pairsum(int *arr,int startindex,int endindex, int num)
+int pairSum(int *arr,int startIndex,int endIndex,int num)
 {
-    int numpair= 0;
-    while(startindex<endindex)
-    {
-            if(arr[startindex]+arr[endindex]<num)
-            startindex++;
-            else if(arr[startindex]+arr[endindex] > num)
-            endindex--;
-            else
-                {
-                    int elementatstart=arr[startindex];
-                    int elementatend=arr[endindex];
-                    
-                }
-    }
+int numPair=0;
+while(startIndex<endIndex)
+{
+	if(arr[startIndex]+arr[endIndex] <num)
+	startIndex++;
+	else if(arr[startIndex]+arr[endIndex]>num)
+	endIndex--;
+	else{
+	int elementAtStart=arr[startIndex];
+	int elementAtEnd=arr[endIndex];
+	
+	if(elementAtStart == elementAtEnd)
+	{
+	int totalElementsFromStartToEnd=(endIndex-startIndex)+1;
+	numPair += (totalElementsFromStartToEnd * (totalElementsFromStartToEnd-1)/2);
+	return numPair;
+	}
+	int tempStartIndex=startIndex+1;
+	int tempEndIndex=endIndex-1;
+	
+	while(tempStartIndex <= tempEndIndex && arr[tempEndIndex] == elementAtEnd
+	tempEndIndex-=1;
+	
+	int totalElementsFromStart = (tempStartIndex
+	-startIndex);
+	int totalElementsFromEnd=(endIndex-tempEndIndex);
+
+	numPair+=(totalElementsFromStart*totalElementsFromEnd);
+
+	startIndex=tempStartIndex;
+	endIndex=tempEndIndex;
+	}
+}
+return numPair;
+}
+
+int tripletSum(int *arr,int n,int num)
+{
+	sort(arr,arr+n);
+	int numTriplets =0;
+	for(int i=0;i<n;i++)
+	{
+		int pairSumFor=num-arr[i];
+		int numPairs= pairSum(arr,(i+1),(n-1),pairSumFor);
+		numTriplets+=numPairs;
+	}
+	return numTriplets;
 }
