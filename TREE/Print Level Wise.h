f785de3c85ad5c62eadee7a1ba7ed84043063a88
Ravi Kumar <ravi14577@gmail.com>
2021-01-10 11:36:45 +0530

    tree

---
+++ TREE/Print Level Wise.h
@@ -0,0 +1,109 @@
+/*
+Print Level Wise
+Send Feedback
+Given a generic tree, print the input tree in level wise order.
+For printing a node with data N, you need to follow the exact format -
+N:x1,x2,x3,...,xn
+where, N is data of any node present in the generic tree. x1, x2, x3, ...., xn are the children of node N. Note that there is no space in between.
+You need to print all nodes in the level order form in different lines.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.
+Output Format :
+The first and only line of output contains the elements of the tree in level wise order, as described in the task.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+10:20,30,40
+20:40,50
+30:
+40:
+40:
+50:
+
+
+*/
+
+
+printLevelWise
+
+void printLevelWise(TreeNode<int>* root) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Print output as specified in the question.
+     */
+  queue <TreeNode<int>*> pn;
+    pn.push(root);
+    
+    while(!pn.empty()){
+        TreeNode <int>* cur=pn.front();
+        pn.pop();
+        
+        cout<<cur->data<<":";
+        
+        for(int i=0;i<cur->children.size();i++){
+            if(i==cur->children.size()-1)
+                cout<<cur->children[i]->data;
+            else
+                cout<<cur->children[i]->data<<",";
+            pn.push(cur->children[i]);
+        }
+        cout<<endl;
+    }
+    
+}
+
+//main code
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    printLevelWise(root);
+}
