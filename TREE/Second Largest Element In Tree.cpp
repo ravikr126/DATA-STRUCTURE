8ad02206b427003d5229a94579d158e9df418772
Ravi Kumar <ravi14577@gmail.com>
2021-01-28 09:36:25 +0530

    Second Largest Element In Tree

---
+++ TREE/Second Largest Element In Tree.cpp
@@ -0,0 +1,133 @@
+/*
+
+Second Largest Element In Tree
+Send Feedback
+Given a generic tree, find and return the node with second largest value in given tree.
+Note: Return NULL if no node with required value is present.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.
+Output Format :
+The first and only line of output contains data of the node with second largest data.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1 :
+40
+
+*/
+
+// Following is the given Tree node structure
+/**************
+
+ template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+***************/
+
+TreeNode <int>* getSecondLargestNode(TreeNode<int> *root) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+   
+    static TreeNode <int>* max = new TreeNode<int>(0);
+      static   TreeNode <int>* n = new TreeNode <int>(0);
+    
+    if(root->data>max->data)
+    {
+       	n->data = max->data;
+        max->data = root->data;
+    }
+     if(root->data>n->data&&root->data!=max->data)
+    {
+        n->data = root->data;
+    }
+    for(int i = 0;i<root->children.size();i++)
+    {
+        getSecondLargestNode(root->children[i]);
+    }
+   
+    if(n->data==0)
+        return NULL;
+    else return n;
+    return n;
+    
+}
+
+//main code
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+
+    TreeNode<int>* ans = getSecondLargestNode(root);
+
+    if (ans != NULL) {
+        cout << ans->data;
+    } else {
+        cout << INT_MIN;
+    }
+
+}
