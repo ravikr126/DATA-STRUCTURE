4c101ace52dbe8dbe96f28fd06835defc9d86d5c
Ravi Kumar <ravi14577@gmail.com>
2021-03-04 19:00:23 +0530

    tree

---
+++ TREE/Count nodes.cpp
@@ -0,0 +1,104 @@
+/*
+
+Count nodes
+Send Feedback
+Given a tree and an integer x, find and return the number of nodes which contains data greater than x.
+Input format:
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+The following line contains an integer, that denotes the value of x. 
+Output Format :
+The first and only line of output prints the count of nodes greater than x.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0
+35 
+Sample Output 1 :
+3
+Sample Input 2 :
+10 3 20 30 40 2 40 50 0 0 0 0
+10 
+Sample Output 2:
+5
+
+*/
+
+
+
+int getLargeNodeCount(TreeNode<int> *root, int x) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+
+
+    int count=0;
+    if(root==NULL)
+        return 0;
+    if(root->data>x)
+        count++;
+    for(int i=0;i<root->children.size();i++){
+        count += getLargeNodeCount(root->children[i],x);
+    }
+    return count;
+}
+
+
+
+
+//main code
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    int x;
+    cin >> x;
+    cout << getLargeNodeCount(root, x);
+}
