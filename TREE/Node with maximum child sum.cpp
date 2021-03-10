54b4e94cdaf9104eea7e4691098b119b9041d040
Ravi Kumar <ravi14577@gmail.com>
2021-03-10 17:01:40 +0530

    tree

---
+++ TREE/Node with maximum child sum.cpp
@@ -0,0 +1,131 @@
+/*
+
+
+Node with maximum child sum
+Send Feedback
+Given a generic tree, find and return the node for which sum of its data and data of all its child nodes is maximum. In the sum, data of the node and data of its immediate child nodes has to be taken.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space. 
+Output format :
+The first and only line of output contains the data of the node with maximum sum, as described in the task.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+5 3 1 2 3 1 15 2 4 5 1 6 0 0 0 0
+Sample Output 1 :
+1
+
+*/
+
+
+// Following is the given Tree node structure
+/**************
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+***************/
+# include <queue>
+TreeNode<int>* maxSumNode(TreeNode<int> *root){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+     queue<TreeNode<int>*> pending;
+    if(root==NULL)
+        return root;
+    pending.push(root);
+    int ans=0;
+    TreeNode<int>* ansNode=root;
+    while(pending.size()!=0){
+        int roots = pending.front()->data;
+        int temp=0;
+        for(int i = 0;i<pending.front()->children.size();i++){
+            temp+= pending.front()->children[i]->data;
+            pending.push(pending.front()->children[i]);
+        }
+        roots = temp+roots;
+        if(roots>ans){
+            ans=roots;
+            ansNode = pending.front();
+        }
+        pending.pop();
+    }
+    return ansNode;
+
+}
+
+
+
+//main code
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+
+    TreeNode<int>* ans = maxSumNode(root);
+
+    if (ans != NULL) {
+        cout << ans->data;
+    }
+}
